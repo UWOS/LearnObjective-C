a4a68db203be5a1fd24cee271ec812d307419c63
UWOS2009@163.com <UWOS@chujianxunchangdeMacBook-Pro.local>
2016-04-30 18:49:08 +0800

    after chapter 22.

--- BMITime/BMITime/BNREmployee.m
+++ BMITime/BMITime/BNREmployee.m
@@ -9,6 +9,16 @@
 #import "BNREmployee.h"
 #import "BNRAsset.h"
 
+// 类扩展
+@interface BNREmployee()
+{
+    NSMutableArray *_assets;
+}
+
+@property (nonatomic) unsigned int officeAlarmCode;
+
+@end
+
 @implementation BNREmployee
 
 - (double) yearsOfEmployment
