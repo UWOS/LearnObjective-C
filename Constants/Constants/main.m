feb3f11463113cd11ecaf990e0e0e2ceb2884509
UWOS2009@163.com <UWOS@chujianxunchangdeMacBook-Pro.local>
2016-05-02 18:34:13 +0800

    after chapter 24.

---
+++ Constants/Constants/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by 初见寻常 on 16/5/2.
+//  Copyright © 2016年 初见寻常. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
