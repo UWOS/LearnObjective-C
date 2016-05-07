927f130f89feb81d937e47da35b07c0373c89645
UWOS2009@163.com <UWOS@chujianxunchangdeMacBook-Pro.local>
2016-05-07 15:49:16 +0800

    added error message.

--- Stringz/Stringz/main.m
+++ Stringz/Stringz/main.m
@@ -16,12 +16,32 @@ int main(int argc, const char * argv[]) {
             [str appendString:@"Aaron is cool!\n"];
         }
         
-        [str writeToFile:@"/tmp/cool.txt"
+        // 声明一个指向NSError对象的指针，但是不创建相应的对象
+        // 实际上，只有当发生错误时，才会由writeToFile创建相应的NSError对象
+        
+        NSError *error;
+        
+        // 将NSError指针通过引用传入writeToFile:automically:encoding:error方法
+        
+        BOOL success = [str writeToFile:@"/tmp/cool.txt"
+                             atomically:YES
+                               encoding:NSUTF8StringEncoding
+                                  error:&error];
+        
+        /*[str writeToFile:@"/tmp/cool.txt"
                atomically:YES
                  encoding:NSUTF8StringEncoding
-                   error:NULL];
+                   error:NULL];*/
         
-        NSLog(@"done writing /tmp/cool.txt");
+        // 检索返回的布尔值，如果写入文件失败，就查询NSError对象并输出错误描述
+        
+        if (success) {
+            NSLog(@"done writing /tmp/cool.txt");
+        } else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
+        
+        
         
     }
     return 0;
