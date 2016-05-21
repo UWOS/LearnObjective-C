b015594b04ab182a6444111274f863e05982e789
UWOS2009@163.com <UWOS@chujianxunchangdeMacBook-Pro.local>
2016-05-21 17:59:43 +0800

    Modified after exercise 28.1 .

--- VowelMovement/VowelMovement/main.m
+++ VowelMovement/VowelMovement/main.m
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-typedef void (^ArrayEnumerationBlock)(id, NSUInteger, BOOL *);
+//typedef void (^ArrayEnumerationBlock)(id, NSUInteger, BOOL *);   // exce 28.1
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -24,10 +24,10 @@ int main(int argc, const char * argv[]) {
         
         // 声明Block变量
         //void (^devowelizer)(id, NSUInteger, BOOL *);
-        ArrayEnumerationBlock devowelizer;
+        //ArrayEnumerationBlock devowelizer;   // exce 28.1
         
         // 将Block对象赋给变量
-        devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+        /*devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
             NSRange yRange = [string rangeOfString:@"y" options:NSCaseInsensitiveSearch];
             
             // 是否包含字符'y'？
@@ -49,9 +49,32 @@ int main(int argc, const char * argv[]) {
             
             [devowelizedStrings addObject:newString];
         };   // Block变量赋值结束
+        */   // exce 28.1
         
         // 枚举数组对象，针对每个数组中的对象，执行Block对象devowelizer
-        [originalStrings enumerateObjectsUsingBlock:devowelizer];
+        [originalStrings enumerateObjectsUsingBlock: /* devowelizer   exce 28.1 */
+         ^(id string, NSUInteger i, BOOL *stop) {
+            NSRange yRange = [string rangeOfString:@"y" options:NSCaseInsensitiveSearch];
+            
+            // 是否包含字符'y'？
+            if (yRange.location != NSNotFound) {
+                *stop = YES;   // 执行完当前的Block对象后终止枚举过程
+                return;   // 结束当前正在执行的Block对象
+            }
+            
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            // 枚举数组中的字符串，将所有出现的元音字母替换成空字符串
+            for (NSString *s in vowels) {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:s
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+            }
+            
+            [devowelizedStrings addObject:newString];
+        }/* exce 28.1 */];
         NSLog(@"new strings: %@", devowelizedStrings);
         
     }
